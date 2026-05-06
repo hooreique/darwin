6886139de3481c6f1cc9d345aa6be9e6e4421cb9
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2026-05-07 00:52:38 +0900

    add missing substituters

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -9,9 +9,11 @@
   environment.etc."nix/nix.custom.conf".text = ''
     trusted-users = ${main-username}
 
+    extra-substituters = https://hooreique.cachix.org
     extra-trusted-substituters = https://hooreique.cachix.org
     extra-trusted-public-keys = hooreique.cachix.org-1:xuPFUhHZkm48tim3zma5/v67Fag5vn8XLBXLiYYeXOE=
 
+    extra-substituters = https://cache.numtide.com
     extra-trusted-substituters = https://cache.numtide.com
     extra-trusted-public-keys = niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=
   '';
